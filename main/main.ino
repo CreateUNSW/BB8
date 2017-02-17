0e914f425261c1817dfa1a2213d84e72007d94d4
Jabez Wilson <jabez.wilson0@gmail.com>
2017-02-17 22:14:46 +1100

    added code for all 5 PIDs and 2 servos

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,5 @@
+
+#include <Servo.h>
 #include <PID_v1.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
@@ -6,12 +8,12 @@
 #include <SoftwareSerial.h>
 #include <SabertoothSimplified.h>
 
-#define CH1 20
-#define CH2 21
+#define CH1 2
+#define CH2 3
 #define CH3 18
 #define CH4 19
-#define CH5 2
-#define CH6 3
+#define CH5 20
+#define CH6 21
 
 // CH1 right up-down
 
@@ -22,6 +24,9 @@ int ch1_value_start, ch2_value_start, ch3_value_start, ch4_value_start, ch5_valu
 //PINS and addresses
 #define ST_PITCH_ROLL_PIN 11
 #define ST_HEAD_YAW_PIN 12
+
+#define SERVO_PITCH 9
+#define SERVO_ROLL 10
 
 #define BNO_BODY 0x29
 #define BNO_HEAD 0x28
@@ -43,15 +48,30 @@ int ch1_value_start, ch2_value_start, ch3_value_start, ch4_value_start, ch5_valu
 #define PITCH_I 2
 #define PITCH_D 1
 
+// ROLL PID
+#define HEAD_ROLL_P 2
+#define HEAD_ROLL_I 5
+#define HEAD_ROLL_D 1
+
+// PITCH PID
+#define HEAD_PITCH_P 12
+#define HEAD_PITCH_I 2
+#define HEAD_PITCH_D 1
+
+// PITCH PID
+#define HEAD_YAW_P 12
+#define HEAD_YAW_I 2
+#define HEAD_YAW_D 1
+
 //Define Variables we'll be connecting to
 double rollSetpoint = 0,rollInput,rollOutput;
 double pitchSetpoint = 0,pitchInput,pitchOutput;
+// for head
+double headRollSetpoint = 0,headRollInput,headRollOutput;
+double headPitchSetpoint = 0,headPitchInput,headPitchOutput;
+double headYawSetpoint = 0,headYawInput,headYawOutput;
 
-//everything for Sabre tooth
-SoftwareSerial Pin1(NOT_A_PIN, 11); // RX on no pin (unused), TX on pin 11 (to S1).
-SabertoothSimplified st_pitch_roll(Pin1); // Use SWSerial as the serial port.
-SoftwareSerial Pin2(NOT_A_PIN, ST_HEAD_YAW_PIN); // RX on no pin (unused), TX on pin 11 (to S1).
-SabertoothSimplified st_head_yaw(Pin2); // Use SWSerial as the serial port.
+Servo s_roll, s_pitch;
 
 //PID
 // change the values
@@ -59,6 +79,18 @@ SabertoothSimplified st_head_yaw(Pin2); // Use SWSerial as the serial port.
 PID rollPID(&rollInput, &rollOutput, &rollSetpoint, ROLL_P, ROLL_I, ROLL_D, DIRECT);
 PID pitchPID(&pitchInput, &pitchOutput, &pitchSetpoint, PITCH_P, PITCH_I, PITCH_D, DIRECT);
 
+//head PIDs
+PID headRollPID(&headRollInput, &headRollOutput, &headRollSetpoint, HEAD_ROLL_P, HEAD_ROLL_I, HEAD_ROLL_D, DIRECT);
+PID headPitchPID(&headPitchInput, &headPitchOutput, &headPitchSetpoint, HEAD_PITCH_P, HEAD_PITCH_I, PITCH_D, DIRECT);
+PID headYawPID(&headYawInput, &headYawOutput, &headYawSetpoint, HEAD_YAW_P, HEAD_YAW_I, HEAD_YAW_D, DIRECT);
+
+//everything for Sabre tooth
+SoftwareSerial Pin1(NOT_A_PIN, 11); // RX on no pin (unused), TX on pin 11 (to S1).
+SabertoothSimplified st_pitch_roll(Pin1); // Use SWSerial as the serial port.
+SoftwareSerial Pin2(NOT_A_PIN, ST_HEAD_YAW_PIN); // RX on no pin (unused), TX on pin 11 (to S1).
+SabertoothSimplified st_head_yaw(Pin2); // Use SWSerial as the serial port.
+
+//BNO stuff
 sensors_event_t bodyEvent,headEvent;
 Adafruit_BNO055 bodyBno = Adafruit_BNO055(BNO_BODY,BNO_BODY);
 Adafruit_BNO055 headBno = Adafruit_BNO055(BNO_HEAD,BNO_HEAD);
@@ -66,25 +98,20 @@ Adafruit_BNO055 headBno = Adafruit_BNO055(BNO_HEAD,BNO_HEAD);
 //flag to check if func needs to be called
 bool call = false;
 
-//stuff to handle serial input
-int input = 0;
-bool inputComplete = false;
+////stuff to handle serial input
+//int input = 0;
+//bool inputComplete = false;
 
 //call this every 100ms before any other func
 void funcStart( ) {
   bodyBno.getEvent(&bodyEvent);
   headBno.getEvent(&headEvent);
-  if(inputComplete) {
-    pitchSetpoint = input;
-    input = 0;
-    inputComplete = false;
-  }
 }
 
 // call this after every other func
 void funcEnd() {
-  Serial.print(" > ");
-  Serial.println(pitchSetpoint);
+  Serial.println(" > ");
+  call = false;
 }
 
 void pitchCalc(void)
@@ -92,11 +119,7 @@ void pitchCalc(void)
 
   //NEED TO BE CHANGED ACCORDING TO ORIENTATION
   pitchInput = (float)bodyEvent.orientation.y;
-  Serial.print(pitchInput);
-  Serial.print(" ");
   pitchPID.Compute();
-  Serial.print(pitchOutput);
-  Serial.print(" ");
   st_pitch_roll.motor(PITCH_MOTOR,pitchOutput);
 }
 
@@ -104,9 +127,30 @@ void rollCalc(void) {
   //NEED TO BE CHANGED ACCORDING TO ORIENTATION
   rollInput = bodyEvent.orientation.x;
   rollPID.Compute();
-  Serial.print("SIDE OUTPUT: ");
-  Serial.println(rollOutput);
   st_pitch_roll.motor(ROLL_MOTOR,rollOutput);
+}
+
+void headPitchCalc(void)
+{
+  //NEED TO BE CHANGED ACCORDING TO ORIENTATION
+  headPitchInput = (float)headEvent.orientation.y;
+  headPitchPID.Compute();
+  s_pitch.write(headPitchOutput);
+}
+
+void headRollCalc(void) {
+  //NEED TO BE CHANGED ACCORDING TO ORIENTATION
+  headRollInput = (float)headEvent.orientation.x;
+  headRollPID.Compute();
+  s_roll.write(headRollOutput);
+  
+}
+
+void headYawCalc(void) {
+  //NEED TO BE CHANGED ACCORDING TO ORIENTATION
+  headYawInput = headEvent.orientation.z;
+  headYawPID.Compute();
+  st_head_yaw.motor(HEAD_MOTOR, headYawOutput);
 }
 
 void Interrupt() {
@@ -131,20 +175,24 @@ void setup(void)
  attachInterrupt(digitalPinToInterrupt(CH2),ch2_handler,CHANGE);
  attachInterrupt(digitalPinToInterrupt(CH3),ch3_handler,CHANGE);
  attachInterrupt(digitalPinToInterrupt(CH4),ch4_handler,CHANGE);
- attachInterrupt(digitalPinToInterrupt(CH5),ch5_handler,CHANGE);
- attachInterrupt(digitalPinToInterrupt(CH6),ch6_handler,CHANGE);
+// attachInterrupt(digitalPinToInterrupt(CH5),ch5_handler,CHANGE);
+// attachInterrupt(digitalPinToInterrupt(CH6),ch6_handler,CHANGE);
 
   Pin1.begin(9600);
   Pin2.begin(9600);
-  Serial.begin(9600);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
 
-//  if(!headBno.begin())
-//  {
-//    /* There was a problem detecting the BNO055 ... check your connections */
-//    Serial.print("Ooops, no MAIN BNO detected ... Check your wiring or I2C ADDR!");
-//    while(1);
-//  }
+  Serial.begin(9600);
+  Serial.println("Starting..."); Serial.println("");
+
+  s_roll.attach(SERVO_ROLL);
+  s_pitch.attach(SERVO_PITCH);
+
+  if(!headBno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no MAIN BNO detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
   if(!bodyBno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
@@ -153,37 +201,49 @@ void setup(void)
   }
 
   pitchPID.SetOutputLimits(-127,127);
+  rollPID.SetOutputLimits(-127,127);
+  headYawPID.SetOutputLimits(-127,127);
+
+  headPitchPID.SetOutputLimits(0,180);
+  headRollPID.SetOutputLimits(0,180);
+
   pitchPID.SetMode(AUTOMATIC);
 //  rollPID.SetMode(AUTOMATIC);
+//  headRollPID.SetMode(AUTOMATIC);
+//  headPitchPID.SetMode(AUTOMATIC);
+//  headYawPID.SetMode(AUTOMATIC);
   Timer1.initialize(100000);
   Timer1.attachInterrupt(Interrupt);
 }
 
 
 void loop() {
-  
+
   if(call) {
     funcStart();
     pitchCalc();
 //    rollCalc();
+//    headPitchCalc();
+//    headRollCalc();
+//    headYawCalc();
     funcEnd();
-    call = false;
   }
 }
 
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read();
-    if (inChar == '\n') {
-      inputComplete = true;
-    } else {
-      int digit = inChar - '0';
-      input = input*10 + digit;
-    }
-  }
-}
+//void serialEvent() {
+//  while (Serial.available()) {
+//    // get the new byte:
+//    char inChar = (char)Serial.read();
+//    if (inChar == '\n') {
+//      inputComplete = true;
+//    } else {
+//      int digit = inChar - '0';
+//      input = input*10 + digit;
+//    }
+//  }
+//}
 
+//RF channel handlers
 void ch1_handler() {
   if(digitalRead(CH1) == HIGH)
   {
